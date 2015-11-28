2dbfe6bb7b4ac90a1a9cc11037df251fe140b139
codeworkx <codeworkx@cyanogenmod.org>
2015-11-28 20:33:54 +0100

    [DO NOT MERGE] g4: camera: add wrapper for device with api version 3.2
    Change-Id: Ifa7a77ff5a9eb903cf2146d8ce8c7d6a64a6da4c

--- include/camera/CameraParametersExtra.h
+++ include/camera/CameraParametersExtra.h
@@ -19,9 +19,11 @@ const char CameraParameters::BURST_SHOT_OFF[] = "burst-shot"; \
 const char CameraParameters::BURST_SHOT_ON[] = "burst-shot"; \
 const char CameraParameters::KEY_BURST_SHOT[] = "burst-shot"; \
 const char CameraParameters::KEY_BURST_SHOT_SUPPORTED[] = "burst-shot-supported"; \
+const char CameraParameters::KEY_LGE_CAMERA[] = "lge-camera";
 
 #define CAMERA_PARAMETERS_EXTRA_H \
     static const char BURST_SHOT_OFF[]; \
     static const char BURST_SHOT_ON[]; \
     static const char KEY_BURST_SHOT[]; \
     static const char KEY_BURST_SHOT_SUPPORTED[]; \
+    static const char KEY_LGE_CAMERA[];
