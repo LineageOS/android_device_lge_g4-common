82cc3c29f49af211891d6ba3777cf8f4ebb2e76c
codeworkx <codeworkx@cyanogenmod.org>
2015-11-28 20:33:54 +0100

    g4: camera: add odd camera wrapper
    supports new and legacy hal
    
    can be triggered by property persist.camera.HAL3.enabled
    
    Change-Id: Ifa7a77ff5a9eb903cf2146d8ce8c7d6a64a6da4c

--- system_prop.mk
+++ system_prop.mk
@@ -7,14 +7,15 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     persist.sys.usb.config=mtp
 
 # Camera
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    camera.disable_zsl_mode=1
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.camera.HAL3.enabled=0 \
+    camera2.portability.force_api=1
 
 # Camera add additional i/p buffer in case of encoder DCVS
 PRODUCT_PROPERTY_OVERRIDES += \
     vidc.enc.dcvs.extra-buff-count=2
 
-#Audio   
+#Audio
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.qc.sdk.audio.ssr=false \
     persist.audio.ssr.3mic=false \
