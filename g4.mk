3d72ccbe90939425699c3c1a7069bae4e2f124c3
genesixx <erwanleboucher@gmail.com>
2016-09-11 15:29:50 +0200

    g4 add telephony ext to build
    Change-Id: I0a602371b15683da5b16a3aa153eb80f6b31d562

--- g4.mk
+++ g4.mk
@@ -93,6 +93,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fs_config_files
 
+# Ril
+PRODUCT_PACKAGES += \
+    telephony-ext
+
+PRODUCT_BOOT_JARS += telephony-ext
+
 # Audio
 PRODUCT_PACKAGES += \
     audiod \
@@ -119,7 +125,7 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:system/etc/a2dp_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:system/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:system/etc/usb_audio_policy_configuration.xml \
-    frameworks/av/services/audiopolicy/config/default_volume_tables.xml:system/etc/default_volume_tables.xml 
+    frameworks/av/services/audiopolicy/config/default_volume_tables.xml:system/etc/default_volume_tables.xml
 
 # Charger
 PRODUCT_PACKAGES += \
