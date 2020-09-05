cca25818aa8240e79242b2e8f52f834b3dad9041
pavlaras <theodoroupaulos@gmail.com>
2020-09-05 17:30:28 +0300

    g4-common: gps: Add gps configuration files from caf
    And define correctly the path to conf files in the gps hal
    
    Change-Id: I204140cf9cd95918c91620b86d89a6197b7d0196

--- g4.mk
+++ g4.mk
@@ -129,14 +129,6 @@ PRODUCT_PACKAGES += \
     timekeep \
     TimeKeep
 
-# IZat configuration
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/flp.conf:system/etc/flp.conf \
-    $(LOCAL_PATH)/configs/izat.conf:system/etc/izat.conf \
-    $(LOCAL_PATH)/configs/msap.conf:system/etc/msap.conf \
-    $(LOCAL_PATH)/configs/quipc.conf:system/etc/quipc.conf \
-    $(LOCAL_PATH)/configs/sap.conf:system/etc/sap.conf
-
 # IRSC
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config
@@ -185,6 +177,15 @@ PRODUCT_PACKAGES += \
 # GPS
 PRODUCT_PACKAGES += \
     gps.msm8992
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/flp.conf:system/etc/flp.conf \
+    $(LOCAL_PATH)/configs/gps.conf:system/etc/gps.conf \
+    $(LOCAL_PATH)/configs/izat.conf:system/etc/izat.conf \
+    $(LOCAL_PATH)/configs/msap.conf:system/etc/msap.conf \
+    $(LOCAL_PATH)/configs/quipc.conf:system/etc/quipc.conf \
+    $(LOCAL_PATH)/configs/sap.conf:system/etc/sap.conf \
+    $(LOCAL_PATH)/configs/xtwifi.conf:system/etc/xtwifi.conf
 
 # healthd related
 PRODUCT_PACKAGES += \
