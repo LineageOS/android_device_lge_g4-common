403bd05b2f6c0435beab830107d7169c02469f78
pavlaras <theodoroupaulos@gmail.com>
2020-09-12 09:23:07 +0300

    g4-common: audio: A different platform info for h811
    it's routing the sounds to -tmus. It's not important but it gives
    a separate decibel control inside the mixer_paths.xml
    
    Change-Id: Ia1dd8546f280add74627b42a98c61f76308950dc

--- g4.mk
+++ g4.mk
@@ -111,8 +111,15 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml \
-    $(LOCAL_PATH)/audio/audio_output_policy.conf:system/vendor/etc/audio_output_policy.conf \
-    $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml
+    $(LOCAL_PATH)/audio/audio_output_policy.conf:system/vendor/etc/audio_output_policy.conf
+
+ifeq ($(strip $(TARGET_DEVICE)),h811)
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_platform_info_h811.xml:system/etc/audio_platform_info.xml
+else
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_platform_info_h815.xml:system/etc/audio_platform_info.xml
+endif
 
 # Camera
 PRODUCT_PACKAGES += \
